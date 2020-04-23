40788b18c3cf5ffac5b3b2cd23e6a96ca4deb05e
chandrika-w <chandrika.welivitigodage@gmail.com>
2020-04-23 21:59:15 +1000

    First Commit

---
+++ src/test/java/com/ckw/web/auto/AppTest.java
@@ -0,0 +1,20 @@
+package com.ckw.web.auto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
