9f277d820adb430e511a63ffa967957549893ee8
chandrika-w <chandrika.welivitigodage@gmail.com>
2020-04-25 12:39:09 +1000

    Added junit5 dependencies. Added sample junit tests. Added gitignore file.

---
+++ src/test/java/com/ckw/web/auto/samples/Sample.java
@@ -0,0 +1,11 @@
+package com.ckw.web.auto.samples;
+
+import org.junit.jupiter.api.Test;
+
+public class Sample {
+    @Test
+    public void sampleMethod()
+    {
+        System.out.println("Hello world");
+    }
+}
